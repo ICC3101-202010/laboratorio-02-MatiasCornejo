33a761058d9331c9178152a8124cdef86ee16d34
Matias  Cornejo lazcano <matiascornejolazcano@MacBook-Air-de-Matias.local>
2020-03-30 23:50:31 -0300

    tu mensaje

---
+++ lab2_poo/lab2_poo/Cancion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace lab_2
+{
+    // Clase Person con dos atributos edad y nombre
+    public class Cancion
+    {
+        // Atributos
+
+        public string genero;
+        public string artista;
+        public string album;
+        public string nombre;
+
+
+
+        // Constructor vacío
+        public Cancion()
+        {
+            genero = "Pop";
+            artista = "Ellie Goulding";
+
+            album = "Lights";
+
+            nombre = "Lights";
+
+        }
+
+        // Constructor con parámetros
+        public Cancion(string inputGenero, string inputArtista, string inputAlbum, string inputNombre)
+
+        {
+
+            genero = inputGenero;
+            artista = inputArtista;
+            album = inputAlbum;
+            nombre = inputNombre;
+        }
+
+
+        public string GetNombre()
+        {
+            return nombre;
+
+        }
+        public string GetGenero()
+        {
+            return genero;
+
+        }
+
+        public string GetArtista()
+        {
+            return artista;
+        }
+
+        public string GetAlbum()
+        {
+            return album;
+        }
+
+    }
+}
---
+++ lab2_poo/lab2_poo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab_2
+{
+    class proyecto
+
+    {
+        public static void Main(string[] args)
+        {
+
+
+            // Creamos un objetos de la clase Cancion.
+            Cancion c = new Cancion();
+            // Imprimimos sus datos
+            Console.WriteLine("Genero: " + c.GetGenero() + ", Artista: " + c.GetArtista() + ", Album: " + c.GetAlbum() + ", Nombre: " + c.GetNombre());
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
